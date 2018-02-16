8054fe506c78c2a285f1703aadee9e71442a6146
Elise Shanholtz <eshanhol@umich.edu>
2018-02-16 11:30:00 -0500

    add works

--- rpn.py
+++ rpn.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-    pass
+    stack = list()
+    for token in arg.split():
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            return arg1 + arg2
+    
 
 def main():
     while True:
-        calculate(input('rpn calc> '))
+        print(calculate(input('rpn calc> ')))
 
 if __name__ == '__main__':
     main()
