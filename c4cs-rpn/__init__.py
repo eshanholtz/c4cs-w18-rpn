82a4bef0f711f002a60a4322af77cb962380af52
Elise Shanholtz <eshanhol@umich.edu>
2018-03-06 17:05:11 -0500

    support for coveralls

---
+++ c4cs-rpn/__init__.py
@@ -0,0 +1,5 @@
+"""
+C4cs-rpn package initializer.
+
+Elise Shanholtz <eshanhol@umich.edu>
+"""
---
+++ setup.py
@@ -0,0 +1,12 @@
+"""RPN python package configuration."""
+
+from setuptools import setup
+
+setup(
+    name='c4cs-rpn',
+    version='0.1.0',
+    packages=['c4cs-rpn'],
+    include_package_data=True,
+    author='Elise Shanholtz',
+    author_email='eshanhol@umich.edu',
+)
