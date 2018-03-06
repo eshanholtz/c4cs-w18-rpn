10fe49bfb1a19c6fc3a72bd1ca251e473c5f9e74
Elise Shanholtz <eshanhol@umich.edu>
2018-03-06 17:14:03 -0500

    fixing filenames for consistency

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 setup(
-    name='c4cs-rpn',
+    name='rpn',
     version='0.1.0',
-    packages=['c4cs-rpn'],
+    packages=['rpn'],
     include_package_data=True,
     author='Elise Shanholtz',
     author_email='eshanhol@umich.edu',
